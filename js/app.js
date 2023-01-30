881372a8cfe3f24ca1dcdc91f4d100424f1adce6
longo2002 <lombardo.fra2002@gmail.com>
2023-01-30 17:26:52 +0100

    done & working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
+const password = document.getElementById("password")
 
+password.innerHTML =" Password ";
+
+let nome = prompt("Dimmi come ti chiami");
+let cognome = prompt("E il tuo cognome?");
+let colore = prompt("Ora il tuo colore preferito!");
+let numero = prompt("E il numero!");
+
+password.innerHTML = nome + cognome + colore + numero
